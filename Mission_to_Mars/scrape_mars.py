fb96e16b4954ec1894db8690293b58527fad8f25
MichelleH <voirdire96@gmail.com>
2020-09-21 22:39:05 -0700

    Pull in table from mongo and changed HTML formats

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,5 +1,6 @@
 # Dependencies
 import lxml.html as lh
+import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
 from splinter import Browser
@@ -21,6 +22,9 @@ def scrape_info():
 
     #Run the funciion to get the list of key vaues for the hemisphere name and image url
     marsmission['hemispheres'] = hemispheres()
+
+    #Run the funciion to get the Mars Facts Table
+    marsmission['factsTable'] = facts()
 
     #Initialize browser for the news page
     browser = init_browser()
@@ -105,3 +109,43 @@ def hemispheres():
     browser.quit()
 
     return hemispheres
+
+def facts():
+
+    browser = init_browser()
+
+    # URL of page to be scraped
+    facts_url = "https://space-facts.com/mars/"
+
+    # Retrieve page with the requests module
+    response = requests.get(facts_url)
+
+    # Create a Beautiful Soup object
+    soup = bs(response.text, 'html.parser')
+
+    #Thanks to https://www.pluralsight.com/guides/extracting-data-html-beautifulsoup for some table extraction tips
+    facts_table = soup.find("table", attrs={"class": "tablepress"})
+    facts_table_data = facts_table.tbody.find_all("tr")
+
+    all_facts_data = []
+
+    #Put the all the fact data in a list
+    for item in range(9): #tabel row range is easily made static based on the small size
+        for td in facts_table_data[item].find_all("td"):
+            #Remove all the markup from the text
+            all_facts_data.append(td.text.strip())
+
+    #Split the list with help from stackoverflow.com
+    #Make a list of the fact items in the even indicies
+    fact_item = all_facts_data[::2] 
+
+    #Make a list of the fact data in the odd indicies
+    fact_data = all_facts_data[1::2]
+
+    facts_table_df = pd.DataFrame({'Description':fact_item,'Data':fact_data}).set_index('Description', drop=True)
+    # Use Pandas to convert the data to a HTML table string.
+    factsTable = pd.DataFrame.to_html(facts_table_df)
+
+    browser.quit()
+
+    return factsTable
